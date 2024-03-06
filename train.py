11d6685db20c64bab8649cb798371a01c94dee7c
Ben Boardley <bboardle@purdue.edu>
2024-03-06 09:50:25 -1000

    imbalanced and validation dataloaders

--- train.py
+++ train.py
@@ -1,15 +1,51 @@
 import torch
 from torch import nn, optim, utils
 from torchvision import transforms, datasets
-
+from torch.utils.data import DataLoader, Dataset, WeightedRandomSampler, Subset
+from sklearn.model_selection import train_test_split
+import numpy as np
 from model import myCNN
+from dataloaders import ImbalancedCIFAR10Dataset
+
+def stratified_split(dataset, test_size=0.1):
+    labels = [label for _, label in dataset]
+    train_idx, val_idx = train_test_split(
+        range(len(dataset)),
+        test_size=test_size,
+        random_state=42,
+        stratify=labels,
+        shuffle=True)
+    return train_idx, val_idx
+
+def calculate_class_distribution(loader):
+    class_counts = torch.zeros(10, dtype=torch.int64)
+    for _, labels in loader:
+        for label in labels:
+            class_counts[label] += 1
+    return class_counts
 
 # CIFAR-10 mean and cov values
 mu = (0.5, 0.5, 0.5)
 cov = (0.5, 0.5, 0.5)
 
 train_dataset = datasets.CIFAR10("~/.torch/data", train=True, transform=myCNN.get_input_transform(mu, cov))
-train_loader = utils.data.DataLoader(train_dataset, shuffle=True, batch_size=64)
+
+train_idx, val_idx = stratified_split(train_dataset, test_size=0.001)
+train_subset = Subset(train_dataset, train_idx)
+val_subset = Subset(train_dataset, val_idx)
+
+imbalanced_train_dataset = ImbalancedCIFAR10Dataset(train_subset, dominant_class=2, percentage=0.9)
+
+train_loader = utils.data.DataLoader(imbalanced_train_dataset, shuffle=True, batch_size=64)
+val_loader = utils.data.DataLoader(val_subset, shuffle=False, batch_size=64)
+
+
+class_distribution_train = calculate_class_distribution(train_loader)
+print("Training Dataset Class Distribution:", class_distribution_train)
+
+class_distribution_val = calculate_class_distribution(val_loader)
+print("Validation Dataset Class Distribution:", class_distribution_val)
+
 
 test_dataset = datasets.CIFAR10("~/.torch/data", train=False, transform=myCNN.get_input_transform(mu, cov))
 test_loader = utils.data.DataLoader(test_dataset, shuffle=False, batch_size=64)
