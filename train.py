acc7cdb961068849970fa00934d088115fab1a9b
Parth Patil <parthvin@gmail.com>
2024-03-07 11:28:07 -0500

    Added progressbar and logger

--- train.py
+++ train.py
@@ -5,11 +5,12 @@ from torchvision import datasets
 from torch.utils.data import Subset
 import higher
 import os
+from tqdm import tqdm
 
 from dataloaders import ImbalancedCIFAR10Dataset
 from loss import get_weighted_loss
 from model import myCNN
-from utils import calculate_class_distribution, compute_accuracy, get_run_uuid, stratified_split
+from utils import (create_folders, compute_accuracy, get_run_uuid, stratified_split, setup_logger, calculate_class_distribution)
 
 
 RUN_UUID = get_run_uuid()
@@ -20,15 +21,14 @@ parser.add_argument("-e", "--epoch", type=int, default=50, help="Number of Epoch
 parser.add_argument("-lr", "--learning-rate", type=float, default=0.001, help="Learning Rate")
 parser.add_argument("-s", "--save", action="store_true", default=True, help="Save the model after running")
 parser.add_argument("-l", "--load", type=str, default=None, help="Load the model from path before running")
+parser.add_argument("-ld", "--log_dir", type=str, default='./run_data/log', help='directory of log')
 parser.add_argument("-sp", "--save-path", type=str, default="./run_data", help="Path to save the model and checkpoints")
 parser.add_argument("-c", "--checkpoint", type=int, default=10, help="Set checkpoint interval")
 parser.add_argument("-sc", "--save-checkpoints", action="store_true", default=False, help="Save the model checkpoints while running")
 parser.add_argument("-ca", "--print-checkpoint-accuracy", action="store_true", default=False, help="Print model accuracy for checkpoints")
 args = parser.parse_args()
 
-
-if not os.path.exists(args.save_path):
-    os.makedirs(args.save_path)
+create_folders([args.save_path, args.log_dir])
 
 # CIFAR-10 mean and cov values
 mu = (0.5, 0.5, 0.5)
@@ -44,17 +44,30 @@ imbalanced_train_dataset = ImbalancedCIFAR10Dataset(train_subset, dominant_class
 train_loader = utils.data.DataLoader(imbalanced_train_dataset, shuffle=True, batch_size=64)
 val_loader = utils.data.DataLoader(val_subset, shuffle=False, batch_size=64)
 
-# ----------------- Check the CLass Distributions ---------------------------
-class_distribution_train = calculate_class_distribution(train_loader)
-print("Training Dataset Class Distribution:", class_distribution_train)
-
-class_distribution_val = calculate_class_distribution(val_loader)
-print("Validation Dataset Class Distribution:", class_distribution_val)
-
-
 # ----------------- Load the CIFAR-10 Test Dataset ---------------------------
 test_dataset = datasets.CIFAR10("~/.torch/data", train=False, transform=myCNN.get_input_transform(mu, cov))
 test_loader = utils.data.DataLoader(test_dataset, shuffle=False, batch_size=64)
+
+
+# Setting up the logging modules
+# Log levels
+# Inside Iteration 15
+# Epoch End 35
+# Model save 45
+# Log Save 25
+progressbar = tqdm(range(args.epoch), desc=f"Training Toy Problem Run - {args.name}", ascii=False, dynamic_ncols=True, colour="yellow")
+log_file_path = os.path.join(args.log_dir, f"run_{args.name}.log")
+logger = setup_logger(log_file_path)
+logger.info(args)
+
+
+# ----------------- Check the CLass Distributions ---------------------------
+class_distribution_train = calculate_class_distribution(train_loader)
+logger.info(f"Training Dataset Class Distribution: {class_distribution_train}")
+
+class_distribution_val = calculate_class_distribution(val_loader)
+logger.info(f"Validation Dataset Class Distribution: {class_distribution_val}")
+
 
 cnn_model = myCNN()
 if args.load is not None:
@@ -66,6 +79,7 @@ loss_fn_no_red = nn.CrossEntropyLoss(reduction="none")
 
 
 device = torch.device("cpu")
+logger.info('Training has Started')
 for epoch in range(args.epoch):
     cnn_model.train()
 
@@ -107,17 +121,22 @@ for epoch in range(args.epoch):
         
         optimizer.step()
 
-    print(f"Running epoch: {epoch} | Loss: {weighted_loss.item()}")
+    logger.log(15, f"Epoch: {epoch} | Loss: {weighted_loss.item()}")
+    progressbar.update(1)
     if epoch % args.checkpoint == 0 and epoch != 0:
         if args.print_checkpoint_accuracy:
             compute_accuracy(cnn_model, test_loader)
         if args.save_checkpoints:
             save_path = os.path.join(args.save_path, f"{args.name}_checkpoint_{epoch // args.checkpoint}.pth")
-            print(f"Saving model checkpoint at {save_path}")
+            logger.log(45, f"Saving model checkpoint at {save_path}")
             torch.save(cnn_model.state_dict(), save_path)
 
 compute_accuracy(cnn_model, test_loader)
 if args.save:
     save_path = os.path.join(args.save_path, f"{args.anme}.pth")
-    print(f"Saving model at {save_path}")
+    logger.log(45, f"Saving model at {save_path}")
     torch.save(cnn_model.state_dict(), os.path.join(args.save_path, f"{args.anme}.pth"))
+
+logger.info('Training is finished')
+logger.log(25, "Saved log file " + log_file_path)
+progressbar.close()
